ab52e630ba6f2dc35ebf5d5e413931555e120515
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-07-21 21:31:00 +0530

    Resolved the issue of refresh in the anime details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -4,13 +4,28 @@ import AnimeDetails from "./AnimeDetails";
 
 const Details = () => {
   let location = useLocation();
+  let data;
+  let history;
+  let mainData;
+  let mainHistory;
+  console.log(location.state);
+  if (location.state) {
+    data = location.state.data;
+    history = location.state.history;
+    window.sessionStorage.setItem("data", JSON.stringify(data));
+    window.sessionStorage.setItem("history", JSON.stringify(history));
+    console.log(data);
+
+    mainData = data;
+    mainHistory = history;
+  } else {
+    mainData = JSON.parse(window.sessionStorage.getItem("data"));
+    mainHistory = JSON.parse(window.sessionStorage.getItem("history"));
+  }
 
   return (
     <div className="details">
-      <AnimeDetails
-        data={location.state.data}
-        history={location.state.history}
-      />
+      <AnimeDetails data={mainData} history={mainHistory} />
     </div>
   );
 };
